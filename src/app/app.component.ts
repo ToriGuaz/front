b7e6df958d5dada957a8b774aed60b8a4d08a827
ToriGuaz <florguazzini92@gmail.com>
2023-02-10 15:20:38 -0300

    empiezo desde pc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'front';
+  title = 'Portfolio web Argentina Programa';
+  name = 'Guazzini Maria Florencia'
+  
 }
